e649cd24fc5194fcf075faf9af0733a54df1d87f
季悠然 <youranreus@qq.com>
2022-01-22 10:56:42 +0800

    feat: 用户歌单与歌单详情接口

--- src/utils/request/user/index.js
+++ src/utils/request/user/index.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios'
 import { createApi } from '../index'
-import { REQUEST_BASE_URL, EXAMPLE_URLS } from 'src/utils/const'
+import { REQUEST_BASE_URL } from 'src/utils/const'
 
 const api = createApi(
   axios.create({
@@ -10,10 +10,35 @@ const api = createApi(
   })
 )
 
+const ccookie = localStorage.getItem('cookie') || ''
+
 // 直接使用 QuerySongs() 函数即可
 export const QueryUser = async uid => {
   return await api({
     method: 'GET',
     url: '/user/detail?uid=' + uid,
+    data: {
+      cookie: ccookie,
+    },
+  })
+}
+
+export const QueryPlayList = async uid => {
+  return await api({
+    method: 'post',
+    url: '/user/playlist?uid=' + uid,
+    data: {
+      cookie: ccookie,
+    },
+  })
+}
+
+export const QueryPlayListDetail = async id => {
+  return await api({
+    method: 'post',
+    url: '/playlist/detail?id=' + id,
+    data: {
+      cookie: ccookie,
+    },
   })
 }
