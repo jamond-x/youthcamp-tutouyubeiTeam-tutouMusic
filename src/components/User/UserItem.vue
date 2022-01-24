5675b9ab638e4869c93b4561b8d983abe6c3b1cb
季悠然 <youranreus@qq.com>
2022-01-24 17:53:16 +0800

    feat: 封装UserItem组件

---
+++ src/components/User/UserItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="user-item" :id="aid" @click="jump">
+    <div>
+      <img :src="avatar" />
+      <div class="shadow" :style="'background-image: url(' + avatar + ');'"></div>
+    </div>
+
+    <div class="text-subtitle1 text-center">{{ name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserItem',
+  props: ['avatar', 'aid', 'name'],
+  data() {
+    return {}
+  },
+  methods: {
+    jump() {
+      this.$router.push('/user/' + this.aid)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.user-item {
+  position: relative;
+  width: 17%;
+  margin: 1rem;
+  cursor: pointer;
+  user-select: none;
+}
+
+.user-item img {
+  position: relative;
+  width: 100%;
+  border-radius: 50%;
+  margin-bottom: 0.25rem;
+  z-index: 2;
+}
+
+.shadow {
+  position: absolute;
+  top: 0.5rem;
+  width: 100%;
+  filter: blur(16px) opacity(0.6);
+  transform: scale(0.92, 0.96);
+  z-index: 1;
+  background-size: cover;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  opacity: 0;
+  transition: opacity 0.2s;
+}
+
+.user-item:hover .shadow {
+  opacity: 1;
+}
+</style>
