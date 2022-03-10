1d036ab1504221582f4e1b2c5462a64bbd2a7a95
jamond-x <x2592253600@163.com>
2022-03-10 11:14:55 +0800

    feat: axios另一种封装

---
+++ src/utils/request/axiosCon.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+class Request {
+  instance
+  interceptors
+  constructor(config) {
+    this.instance = axios.create(config)
+    this.instance.interceptors = config.interceptors
+    this.instance.interceptors.request.use(
+      req => {
+        // 全局请求拦截
+        return req
+      },
+      err => err
+    )
+
+    this.instance.interceptors.request.use(this.interceptors.requestInterceptor, err => err)
+    this.instance.interceptors.response.use(this.interceptors.responseInterceptor, err => err)
+
+    this.instance.interceptors.response.use(res => {
+      if (res.data) {
+        return res.data
+      }
+      return res
+    })
+  }
+  request(config) {
+    return this.instance.request(config)
+  }
+}
